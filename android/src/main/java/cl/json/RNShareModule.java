a7b8a84c6d5736d7cc6562d9911b169fc46dc9f2
ibnekhan <ibnekhan@outlook.com>
2019-01-09 19:20:46 +0500

    return callback after share intent closed

--- android/src/main/java/cl/json/RNShareModule.java
+++ android/src/main/java/cl/json/RNShareModule.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.ActivityNotFoundException;
 import android.net.Uri;
 import android.support.annotation.Nullable;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -29,6 +30,9 @@ import cl.json.social.PinterestShare;
 public class RNShareModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
+    public static boolean isOkReturnFromShareIntent = false;
+    public static @Nullable Callback mySuccessCallback;
+
     private enum SHARES {
         facebook,
         generic,
@@ -74,7 +78,7 @@ public class RNShareModule extends ReactContextBaseJavaModule {
 
     @Override
     public String getName() {
-    return "RNShare";
+        return "RNShare";
     }
 
     @javax.annotation.Nullable
@@ -89,15 +93,16 @@ public class RNShareModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void open(ReadableMap options, @Nullable Callback failureCallback, @Nullable Callback successCallback) {
-        try{
+        try {
             GenericShare share = new GenericShare(this.reactContext);
             share.open(options);
-            successCallback.invoke("OK");
-        }catch(ActivityNotFoundException ex) {
+            mySuccessCallback = successCallback;
+//            successCallback.invoke("OK");
+        } catch (ActivityNotFoundException ex) {
             System.out.println("ERROR");
             System.out.println(ex.getMessage());
             failureCallback.invoke("not_available");
-        }catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("ERROR");
             System.out.println(e.getMessage());
             failureCallback.invoke(e.getMessage());
@@ -107,8 +112,8 @@ public class RNShareModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void shareSingle(ReadableMap options, @Nullable Callback failureCallback, @Nullable Callback successCallback) {
         System.out.println("SHARE SINGLE METHOD");
-        if (ShareIntent.hasValidKey("social", options) ) {
-            try{
+        if (ShareIntent.hasValidKey("social", options)) {
+            try {
                 ShareIntent shareClass = SHARES.getShareClass(options.getString("social"), this.reactContext);
                 if (shareClass != null && shareClass instanceof ShareIntent) {
                     shareClass.open(options);
@@ -116,7 +121,7 @@ public class RNShareModule extends ReactContextBaseJavaModule {
                 } else {
                     throw new ActivityNotFoundException("Invalid share activity");
                 }
-            }catch(ActivityNotFoundException ex) {
+            } catch (ActivityNotFoundException ex) {
                 System.out.println("ERROR");
                 System.out.println(ex.getMessage());
                 failureCallback.invoke(ex.getMessage());
@@ -127,18 +132,6 @@ public class RNShareModule extends ReactContextBaseJavaModule {
             }
         } else {
             failureCallback.invoke("key 'social' missing in options");
-        }
-    }
-
-    @ReactMethod
-    public void isPackageInstalled(String packagename, @Nullable Callback failureCallback, @Nullable Callback successCallback) {
-        try{
-            boolean res = ShareIntent.isPackageInstalled(packagename, this.reactContext);
-            successCallback.invoke(res);
-        }
-        catch (Exception e){
-            System.out.println("Error: " + e.getMessage());
-            failureCallback.invoke(e.getMessage());
         }
     }
 
